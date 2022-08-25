b207698673af2590be923437e90fd59ad4f8da6f
iaggocapitanio1 <iaggo.capitanio@gmail.com>
2022-08-25 01:04:58 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import * as React from "react";
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import drfProvider, { jwtTokenAuthProvider, fetchJsonWithAuthJWTToken} from 'ra-data-django-rest-framework';
 
-const authProvider = jwtTokenAuthProvider({
-  obtainAuthTokenUrl: 'http://127.0.0.1:8000/api/v1/obtain-token/'
-})
-const dataProvider = drfProvider("http://127.0.0.1:8000/api/v1", fetchJsonWithAuthJWTToken);
+const API_URL = 'http://127.0.0.1:8000/api/v1';
+
+const authProvider = jwtTokenAuthProvider({ obtainAuthTokenUrl: API_URL + '/obtain-token/'});
+const dataProvider = drfProvider(API_URL, fetchJsonWithAuthJWTToken);
 
 const App = () => (
   <Admin dataProvider={dataProvider} authProvider={authProvider}>
       <Resource name="truck" list={ListGuesser} />
+      <Resource name="producer" list={ListGuesser} />
+      <Resource name="operator" list={ListGuesser} />
+      <Resource name="truck-driver" list={ListGuesser} />
+      <Resource name="loading" list={ListGuesser} />
+      <Resource name="unity" list={ListGuesser} />
+      <Resource name="staff" list={ListGuesser} />
+      <Resource name="company" list={ListGuesser} />
   </Admin>
 );
 
